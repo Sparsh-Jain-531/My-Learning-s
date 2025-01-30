1bd5a9722bee6fac2d46eb259353b6ec4a970c23
Sparsh-Jain-531 <sparshjain531@gmail.com>
2025-01-30 17:36:40 +0530

    Refactor object examples: add product object with methods, demonstrate JSON conversion, and clarify const variable behavior; update HTML to include a subscribe button and implement button click functionality in JavaScript.

--- Web Development/Frontend Development/JavaScript/06_Objects/01_objects.js
+++ Web Development/Frontend Development/JavaScript/06_Objects/01_objects.js
@@ -22,6 +22,10 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
+//Which one to use?
+//use dot notation by default.
+//for properties that don't work,use bracket notation.
+
 JsUser.email = "Sparsh@chatgpt.com"
 // Object.freeze(JsUser)
 JsUser.email = "Sparsh@microsoft.com"
@@ -36,3 +40,61 @@ JsUser.greetingTwo = function(){
 
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
+
+
+const product={
+    name:"shity",
+    ["delivery-time"]:"1 Day",
+    rating:{
+        starts:4.5,
+        reviews:1000
+    },
+    fun: function hello(){
+        console.log("Hello Product Fun")
+    }
+}
+
+console.log(product)
+product.fun()
+
+// convert javascript object to JSON(JSON does not allows functions)
+const json=JSON.stringify(product)
+console.log(json)
+
+//convert JSON to JavaScript object
+const obj=JSON.parse(json)
+console.log(obj)
+
+// const value change in object
+
+const a={
+    val:1,
+}
+
+const b={
+    val:2,
+}
+
+// a=b;
+ // here we try to change the reference of const variable so that's why it gives error of const assign
+
+
+// a.val=b.val; 
+// here we are not changing the reference but instead we change value present in that reference that's why it changes without any error.
+console.log(a)
+
+const c=a;
+console.log(a==c) // here both a and c are pointing to same reference.
+const d={
+    val:1,
+}
+console.log(a==d) // here the reference is different for a and d although the object defination is same.
+
+// const val=a.val
+const {val}=a;  // Destructuring 
+console.log(val)
+
+const e={
+    val,
+}
+console.log(e)
--- Web Development/Frontend Development/Practice/script.js
+++ Web Development/Frontend Development/Practice/script.js
@@ -1,57 +1,10 @@
-function greet(name){
-    if(name){
-        console.log(`Hello, ${name}!`);
+const btn=document.querySelector(".btn");
+console.log(btn)
+
+btn.addEventListener("click",()=>{
+    if(btn.innerHTML==="Subscribe"){
+        btn.innerHTML="Subscribed";
     } else {
-        console.log(`Hello, there`);
+        btn.innerHTML="Subscribe";
     }
-}
-greet("sparsh jain")
-greet()
-
-function temp(deg){
-    return (deg*9/5)+32;
-}
-console.log(temp(25))
-
-function temp1(deg){
-    return (deg-32)*5/9;
-}
-console.log(temp1(86))
-
-function temperature(deg,unit){
-    if(unit==="c"){
-        return (deg*9/5)+32+"F";
-    } else if(unit==="f"){
-        return (deg-32)*5/9+"C";
-    } else {
-        return "Invalid unit";
-    }
-}
-
-console.log(temperature(25,"c"))
-console.log(temperature(86,"f"))
-console.log(temperature(86,"d"))
-
-function convertLength(length, from, to){
-    if(from==="miles" && to==="km"){
-        return length*1.6+" "+to;
-    } else if(from==="km" && to==="miles"){
-        return length/1.6+" "+to;
-    } else if(from==="km" && to==="ft"){
-        return length*3281+" "+to;
-    } else if(from==="miles" && to==="ft"){
-        return length*5280+" "+to;
-    } else if(from===to){
-        return length+" "+to;
-    } else {
-        return "Invalid unit";
-    }
-}
-
-console.log(convertLength(50,"miles","km"))
-console.log(convertLength(32,"km","miles"))
-console.log(convertLength(50,"km","km"))
-
-console.log(convertLength(5,"miles","km"))
-console.log(convertLength(5,"miles","ft"))
-console.log(convertLength(5,"km","ft"))
+})
