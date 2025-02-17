3f1f7e7da068d300fb2f70091896ad77d26f3fac
Sparsh-Jain-531 <sparshjain531@gmail.com>
2025-02-17 17:57:14 +0530

    Implement file management features: add routes for creating, editing, deleting, and displaying files; enhance views for user interaction

--- Web Development/Backend Development/Practice/index.js
+++ Web Development/Backend Development/Practice/index.js
@@ -1,18 +1,51 @@
+const express = require('express')
 const path = require('path')
-console.log(path.join(__dirname,"public"))
+const fs=require('fs');
+const app = express();
 
-// import express from 'express';
-// const app = express();
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname,"public")))
+app.set("view engine","ejs")
 
-// app.use(express.json())
-// app.use(express.urlencoded({extended: true}))
-// app.use(express.static("public"))
-// app.set("view engine","ejs")
+app.get("/",(req,res)=>{
+    fs.readdir("./files",(err,files)=>{
+        res.render("index",{files: files});
+    })
+})
 
-// app.get("/",(req,res)=>{
-//     res.render("index");
-// })
+app.post("/create",(req,res)=>{
+    fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`,req.body.description,(err)=>{
+        res.redirect("/")
+    })  
+})
 
-// app.listen(4000,()=>{
-//     console.log('Server is running on port 4000');
-// })
+app.get("/file/:filename",(req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,filedata)=>{
+        res.render("show",{name:req.params.filename, filedata:filedata});
+    })
+})
+
+app.get("/edit/:filename",(req,res)=>{
+    res.render("edit",{filename:req.params.filename})
+})
+
+app.post("/edit",(req,res)=>{
+    fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,(err)=>{
+        res.redirect("/")
+    })
+})
+
+app.get("/delete/:filename",(req,res)=>{
+    res.render("delete",{filename:req.params.filename})
+})
+
+app.post("/delete",(req,res)=>{
+    fs.rm(`./files/${req.body.filename}`,{ recursive:true }, (err) => {
+        res.redirect("/")
+    })
+})
+
+app.listen(4000,()=>{
+    console.log('Server is running on port 4000');
+})
