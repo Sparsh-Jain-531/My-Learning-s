1163eb2e7ed516e25514e0129217cbb42f9a06ae
Sparsh-Jain-531 <sparshjain531@gmail.com>
2025-04-30 17:51:53 +0530

    feat: enhance TypeScript notes with class, object, and function examples

--- Web Development/Frontend Development/Typescript/app.js
+++ Web Development/Frontend Development/Typescript/app.js
@@ -1,26 +1,13 @@
 "use strict";
-const arr = [1, 2, 3];
-const arr2 = [12, "sparsh"];
-var UserRoles;
-(function (UserRoles) {
-    UserRoles["ADMIN"] = "admin";
-    UserRoles["SUPER_ADMIN"] = "super_admin";
-    UserRoles["USER"] = "user";
-})(UserRoles || (UserRoles = {}));
-function fun(a) {
-    return a;
+function abcd(a, b) {
+    if (typeof a === "string" && b === undefined) {
+        console.log("hey");
+    }
+    if (typeof a === "string" && typeof b === "number") {
+        return 123;
+    }
+    else
+        throw new Error("something went wrong!");
 }
-let value;
-value = "sparsh";
-value = 12;
-// If we define two interface with same name then when we declare that interface to any variable then it shows all peoperties which present in both.(basically they merge properties when they have same name)
-const obj2 = {
-    name: "string",
-    email: "string",
-    password: "string"
-};
-function objj(obj) {
-    return obj;
-}
-console.log(objj({ name: "sparsh", email: "sparsh@gmail.com", password: "12345" }));
-let a;
+abcd("hey");
+abcd("hey", 12);
--- Web Development/Frontend Development/Typescript/app.ts
+++ Web Development/Frontend Development/Typescript/app.ts
@@ -1,54 +1,15 @@
-const arr: number[] = [1, 2, 3];
-const arr2: [number, string] = [12, "sparsh"];
-enum UserRoles {
-  ADMIN = "admin",
-  SUPER_ADMIN = "super_admin",
-  USER = "user",
+function abcd(a: string): void;
+function abcd(a: string, b: number): number;
+
+function abcd(a: any, b?: any){
+    if(typeof a==="string" && b === undefined){
+        console.log("hey")
+    }
+    if(typeof a==="string" && typeof b==="number"){
+        return 123;
+    }
+    else throw new Error("something went wrong!")
 }
 
-function fun(a: number): number {
-  return a;
-}
-
-let value: string | number;
-value="sparsh";
-value=12;
-
-interface User{
-    name: string,
-    email: string,
-    password: string,
-    gender?: string  // optional
-}
-
-// interface extends another interface
-interface Admin extends User{
-    admin: string
-}
-
-// If we define two interface with same name then when we declare that interface to any variable then it shows all peoperties which present in both.(basically they merge properties when they have same name)
-
-const obj2 : User = {
-    name: "string",
-    email: "string",
-    password: "string"
-}
-
-function objj(obj: User) : User{
-    return obj;
-}
-
-console.log(objj({name: "sparsh", email:"sparsh@gmail.com", password: "12345"}))
-
-type value = string | number | boolean;
-let a :value;
-
-type User1={
-    name: string,
-    email: string
-}
-
-type Admin1 = User1 & {
-    getDetails(user: string): void
-}
-
+abcd("hey")
+abcd("hey",12)
